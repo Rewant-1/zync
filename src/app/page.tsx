e06351ded17e2f866e3433bccf69a1c20f9eaf83
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-07-01 14:24:58 +0530

    Background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { trpc, getQueryClient } from "@/trpc/server";
-import { HydrationBoundary,dehydrate } from "@tanstack/react-query";
-import {Client} from "./client";
-import { Suspense } from "react";
-const Page = async () => {
-const queryClient=getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Rewant"}))
+"use client";
+const Page =  () => {
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading bruv..</p>}><Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div></div>
   );
 }
 

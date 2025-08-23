6e2b01ec08c1cb6d62c39a4269029abc1ea62224
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-08-23 11:07:16 +0530

    finally over

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -75,7 +75,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "An expert React coding agent",
       system: PROMPT,
       model: openai({
-        model: "qwen/qwen3-4b:free",
+        model: "z-ai/glm-4.5-air:free",
         baseUrl: "https://openrouter.ai/api/v1",
         apiKey: openRouterApiKey,
       }),
